b00fa6fd735440306b532a38ec6bab5c6019c59c
KarolinaBuceneka <144551825+KarolinaBuceneka@users.noreply.github.com>
2024-01-18 15:26:03 +0200

    Update projekts.py

--- projekts.py
+++ projekts.py
@@ -70,3 +70,34 @@ for produkt in range(len(info_list)):
                         else:  
                             break  
                     time.sleep(10)
+
+            except Exception:
+                close_button = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/button')
+                close_button.click()
+                time.sleep(1)
+                search_button= driver.find_element(By.XPATH, "/html/body/header/div[2]/div[3]/div[1]/form/label/div/button") 
+                search_button.click()   
+                time.sleep(1) 
+        
+                possible_product = driver.find_element (By.XPATH, '//*[@id="main"]/section/div[1]/div/div[2]/div[1]/div/div[2]/ul/li[1]/div/a') 
+                possible_product.click()  
+                time.sleep(1) 
+        
+                find = driver.find_element(By.XPATH, '//*[@id="main"]/section/div[1]/div/div[2]/section/div/div/div[2]/h1')   
+                x=find.text    
+                time.sleep(1) 
+
+                if product_name.lower() in str(x.lower()): 
+                    add_to_cart = driver.find_element(By.XPATH, '//*[@id="main"]/section/div[1]/div/div[2]/section/div/div/div[2]/div[2]/form[1]/button') 
+                    add_to_cart.click()  
+                    n=1
+                    float_product_amount=float(product_amount)
+                    for m in range(int(product_amount)):
+                        if float_product_amount>1 and n < float_product_amount:
+                            add_more=driver.find_element(By.XPATH, '//*[@id="main"]/section/div[1]/div/div[2]/section/div/div/div[2]/div[2]/div/form/button[2]') 
+                            add_more.click() 
+                            n=n+1
+                            time.sleep(2)
+                        else:
+                            break
+                    time.sleep(10)
