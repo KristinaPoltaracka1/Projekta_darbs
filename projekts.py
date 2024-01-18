35ab3c070a33fe57dc596b84ebb85f11d970f7eb
Kristina231RDB084 <144888347+KristinaPoltaracka1@users.noreply.github.com>
2024-01-18 15:18:44 +0200

    Update projekts.py

--- projekts.py
+++ projekts.py
@@ -1,2 +1,6 @@
-import selenium 
 from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys 
+import time 
+import pandas 
