46f02c15ad0cae13b48bda1d8d7572e7399fcca5
KarolinaBuceneka <144551825+KarolinaBuceneka@users.noreply.github.com>
2024-01-18 15:22:03 +0200

    Update projekts.py

--- projekts.py
+++ projekts.py
@@ -4,3 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys 
 import time 
 import pandas 
+service = Service() 
+option = webdriver.ChromeOptions() 
+driver = webdriver.Chrome(service=service, options=option) 
+
+url = "https://www.rimi.lv/e-veikals" 
+driver.get(url) 
+time.sleep(2) 
+accept_cookies = driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll") 
+accept_cookies.click() 
+s=[] 
+
+fails = pandas.read_excel("Produkti.xlsx", sheet_name="Sheet1")  
+info_list = fails.values.tolist() 
+match=False 
